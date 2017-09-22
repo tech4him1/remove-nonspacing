f5d7407a83f09d2d61d852a2438069f0d1b9e91b
Caleb <inchristalway-tech4him@yahoo.com>
2017-09-22 09:25:58 -0600

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var nonspacing = new Set(require('./nonspacing'));
+
+function removeAccents(string) {
+  var decomposed = string.normalize("NFD");
+  var runes = decomposed.split('');
+  var cleaned = runes.map(function (r) {
+    var charCode = r.charCodeAt(0);
+    // Drop non-spacing chars.
+    if (nonspacing.has(charCode)) {
+      return;
+    }
+    // Keep all other chars.
+    return r;
+  });
+  var composed = cleaned.join('').normalize("NFC");
+  return composed;
+}
+
+module.exports = removeAccents;
